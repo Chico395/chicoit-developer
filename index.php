4941de18bd623f1e4c79297bce28bf66501c0a06
Chico395 <143083369+Chico395@users.noreply.github.com>
2024-01-23 14:16:07 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200&family=Open+Sans&family=Roboto:ital,wght@0,300;1,100;1,400&display=swap" rel="stylesheet">
+    <link rel="" href="">
+<link rel="" href="" crossorigin="" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="" href="" crossorigin>
+<link href="" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chicoit developer</title>
+    <link rel="stylesheet" href="design.css">
+</head>
+<body>
+   <center>
+   <div class="main">
+   <form action="insert.php" method="post">
+        <h2>web-development services</h2>
+        <img src="background.png" alt="logo" width="450px">
+        <input type="text" name='name'>
+        <br>
+        <input type="text" name='price'>
+        <br>
+        <input type="file" id="file" name="image" style='display:none;'>
+        <label for="file">select product image</label>
+        <button name='upload'>upload product</button>
+        <br><br>
+        <a href="products.php">show all products</a>
+   </form>
+   </div>
+   <p>Engined By Chico</p>
+   </center>
+</body>
+</html>
+<div id="main"></div>
